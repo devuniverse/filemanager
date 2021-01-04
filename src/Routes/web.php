5e65526afaeb7bad24a2a2c48edfce39f41a4461
devuniverse <ishimwe@gmail.com>
2021-01-03 21:46:51 -0500

    Update web.php

--- src/Routes/web.php
+++ src/Routes/web.php
@@ -3,19 +3,19 @@
 /**
  * GET ROUTES
  */
-Route::get('/', 'Devuniverse\Filemanager\Controllers\FilemanagerController@loadIndex');
-Route::get('create', 'Devuniverse\Filemanager\Controllers\FilemanagerController@create')->name('filemanager.create');
-Route::get('showfiles', 'Devuniverse\Filemanager\Controllers\FilemanagerController@index')->name('load.filemanager.index');
-Route::get('modaluploader', 'Devuniverse\Filemanager\Controllers\FilemanagerController@loadGallery')->name('load.filemanager.gallery');
-Route::get('modalcropper', 'Devuniverse\Filemanager\Controllers\FilemanagerController@getModalCropper');
+Route::get('/', 'Devuniverse\Filemanager\Controller\FilemanagerController@loadIndex');
+Route::get('create', 'Devuniverse\Filemanager\Controller\FilemanagerController@create')->name('filemanager.create');
+Route::get('showfiles', 'Devuniverse\Filemanager\Controller\FilemanagerController@index')->name('load.filemanager.index');
+Route::get('modaluploader', 'Devuniverse\Filemanager\Controller\FilemanagerController@loadGallery')->name('load.filemanager.gallery');
+Route::get('modalcropper', 'Devuniverse\Filemanager\Controller\FilemanagerController@getModalCropper');
 
 
-Route::post('/file-save', 'Devuniverse\Filemanager\Controllers\FilemanagerController@store');
-Route::post('/file-delete', 'Devuniverse\Filemanager\Controllers\FilemanagerController@destroy');
-Route::post('/delete-files', 'Devuniverse\Filemanager\Controllers\FilemanagerController@deleteFiles');
+Route::post('/file-save', 'Devuniverse\Filemanager\Controller\FilemanagerController@store');
+Route::post('/file-delete', 'Devuniverse\Filemanager\Controller\FilemanagerController@destroy');
+Route::post('/delete-files', 'Devuniverse\Filemanager\Controller\FilemanagerController@deleteFiles');
 
 /**
  * POST ROUTES
  */
- Route::post('/modaluploader', 'Devuniverse\Filemanager\Controllers\FilemanagerController@modalUploader');
- Route::post('/modalcropper', 'Devuniverse\Filemanager\Controllers\FilemanagerController@modalCropper');
+ Route::post('/modaluploader', 'Devuniverse\Filemanager\Controller\FilemanagerController@modalUploader');
+ Route::post('/modalcropper', 'Devuniverse\Filemanager\Controller\FilemanagerController@modalCropper');
